edacec7d1dbd1a7538ab91c8ff4aec4c18e53d06
sarahimrancs1091ntu-prog <sarahimrancs1091ntu@gmail.com>
2026-09-25 11:00:53 +0500

    Add files via upload

---
+++ Task 1/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#define LED_BUILTIN 2
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
+
